af4919206185a0271793b84afbcb118ade3490f4
Magdaline Sisungo <47854471+magdalinesisungo@users.noreply.github.com>
2019-04-02 21:04:11 +0300

    Add files via upload
    added a python file

---
+++ example 2/calculator.py
@@ -0,0 +1,32 @@
+def add(a,b):
+	c=a+b
+	print(c)
+
+
+def subtract(a,b):
+	c=a-b
+	print(c)
+
+def multiply(a,b):
+	c=a*b
+	print(c)
+
+def divide(a,b):
+	c=a/b
+	print(c)
+
+def modulus(a,b):
+	c=a%b
+	print(c)
+
+def percentage(a,b):
+	c=a/b*100
+	print(c)
+
+def whole_number_division(a,b):
+	c=a//b
+	print(c)
+
+def exponent(a,b):
+	c=a**b
+	print(c)
