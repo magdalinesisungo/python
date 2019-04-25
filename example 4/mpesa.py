f0e3c7fe78a96da226efee2d1a4abcd067dfe0a4
Magdaline Sisungo <47854471+magdalinesisungo@users.noreply.github.com>
2019-04-25 17:13:24 +0300

    Add files via upload

--- example 4/mpesa.py
+++ example 4/mpesa.py
@@ -1,22 +1,43 @@
-class Mpesa:
-
-	def __init__(self,full_name,phone_number,balance):
-		self.full_name = full_name
+class MpesaAccount:
+	def __init__(self, name, phone_number):
+		self.name = name
 		self.phone_number = phone_number
-		self.balance = balance
+		self.balance = 0
+		self.deposits = []
+		self.withdraws = []
+		self.loan = 0
+		 
+	def deposit(self,amount):
+		self.balance =  amount + self.balance
+		self.deposits.append(amount)
+		sms1 ="hello {}, you have deposited {} your balance is {}".format(self.name,amount,self.balance)
+		print(sms1) 
+		
 
-	def deposit_amount(self,m):
-		deposit_amount = m + self.balance
-		self.balance = self.balance + m
-        sms1= "Dear {} you have deposited {} ksh, your current balance is {} ksh".format(self.full_name,m,self.balance)
-        print (sms1)
-
-	def withdraw(self,m):
-		withdraw = self.balance - m
-		sms2= "Dear {} you have withdrawn{} ksh, your curent balance is {} ksh".format(self.full_name,m,self.balance)
-		print (sms2)
+	def withdraw(self, amount):
+		if amount<self.balance:
+			self.balance =  self.balance-amount
+			self.withdraws.append(amount)
+			sms2= "hello {}, you have withdrawn {} your balance is {}".format(self.name,amount,self.balance)
+			print (sms2)
+		else:
+			print("insufficient balance")	
+			
 
 	def check_balance(self):
-		check_balance = balance
-		sms3= "Dear {} your current balance is {} ksh".format(self.full_name,m,self.balance)
-		print (sms3)
+		sms3="hello {}, your current balance is {}".format(self.name, self.balance)
+		print(sms3)
+
+	def my_deposits(self):
+		for k in self.deposits:
+			print(k)
+
+	def my_withdraws(self):
+		for s in self.withdraws:
+			print(s)
+
+	def give_loans(self,amount):
+		if 
+		
+			
+
