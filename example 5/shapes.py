abc2b2e2aebd9b74202e1981ff0fca3f4f2af8a4
Magdaline Sisungo <47854471+magdalinesisungo@users.noreply.github.com>
2019-04-30 19:29:30 +0300

    added new file

---
+++ example 5/shapes.py
@@ -0,0 +1,63 @@
+
+class Circle:
+
+	def __init__(self,Radius,Area,Circumference):
+		self.Radius = r
+		self.Area = A
+		self.Circumference = C
+
+	def Area(self,radius):
+		self.Area = 3.14*(r**2)
+		print(Area)
+
+	def Circumference(self,radius):
+		self.Circumference = 2*3.14*r
+		print(Circumference)
+
+
+class Square:
+
+	def __init__(self,side,Area,Perimeter):
+		self.side = a
+		self.Area = A
+		self.Perimeter = P
+
+	def Area(self,side):
+		self.Area = a**2
+		print(A)
+
+	def Perimeter(self,side):
+		self.Perimeter = 4*a
+		print(P)
+
+
+class Rectangle:
+
+	def __init__(self,sides,Area,Perimeter):
+		self.sides = W,l
+		self.Area = A
+		self.Perimeter = P
+
+	def Area(self,sides):
+		self.Area = W * l
+		print(A)
+
+	def Perimeter(self,sides):
+		self.Perimeter = 2(l + W)
+		print(P)
+
+class Sphere:
+
+	def __init__(self,radius,Area,Volume):
+		self.radius = radius
+		self.Area = A
+		self.Volume = V
+
+	def Area(self,radius):
+	 	self.Area = 4*3.14*(r**2)
+	 	print(A)
+
+	def Volume(self,radius):
+	 	self.Volume = 4/3(3.14*(r**3))
+	 	print(V)
+		 
