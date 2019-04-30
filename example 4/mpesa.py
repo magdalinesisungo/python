902184fa0369db026aa0b056bf136f41f3d66b3d
Magdaline Sisungo <47854471+magdalinesisungo@users.noreply.github.com>
2019-04-30 16:07:20 +0300

    Add files via upload

--- example 4/mpesa.py
+++ example 4/mpesa.py
@@ -1,3 +1,5 @@
+
+from datetime import datetime
 class MpesaAccount:
 	def __init__(self, name, phone_number):
 		self.name = name
@@ -9,7 +11,9 @@ class MpesaAccount:
 		 
 	def deposit(self,amount):
 		self.balance =  amount + self.balance
-		self.deposits.append(amount)
+		now = datetime.now()
+		object= {"time":now,"amount":amount}
+		self.deposits.append(object)
 		sms1 ="hello {}, you have deposited {} your balance is {}".format(self.name,amount,self.balance)
 		print(sms1) 
 		
@@ -17,7 +21,9 @@ class MpesaAccount:
 	def withdraw(self, amount):
 		if amount<self.balance:
 			self.balance =  self.balance-amount
-			self.withdraws.append(amount)
+			now = datetime.now()
+			object= {"time":now,"amount":amount}
+			self.withdraws.append(object)
 			sms2= "hello {}, you have withdrawn {} your balance is {}".format(self.name,amount,self.balance)
 			print (sms2)
 		else:
@@ -29,12 +35,16 @@ class MpesaAccount:
 		print(sms3)
 
 	def my_deposits(self):
-		for k in self.deposits:
-			print(k)
+		for deposit in self.deposits:
+			time = deposit["time"].strftime("%A %B %d %Y")
+			amount = deposit["amount"]
+			print("hello {}, on date {} you deposited {}".format(self.name,time,amount))
 
 	def my_withdraws(self):
-		for s in self.withdraws:
-			print(s)
+		for withdraw in self.withdraws:
+			time = withdraw["time"].strftime("%A %B %d %Y")
+			amount = withdraw["amount"]
+			print("hello {}, on date {} you withdrew {}".format(self.name,time,amount))
 
 	def give_loans(self,amount):
 		if len(self.deposits)>=5 and amount<1/3*sum(self.deposits) and self.loan==0:
@@ -51,12 +61,24 @@ class MpesaAccount:
 			print("Hello {}you have paid part of your loan,{}. your remaining balance is {}".format(self.name ,amount,self.loan))
 		elif amount==self.loan:
 			self.loan=self.loan-amount
-			print("you have paid your all your loan")
+			print("you have paid all your loan")
 		elif amount>self.loan:
 			more=amount-self.loan
 			self.balance= more+self.balance
 			self.loan=amount-self.loan-more
 			print("Hello {} you have paid more than is required, your new balance is{}".format(self.name,self.balance))
+
+	def statement(self):
+		for b in self.withdraws:
+			time=b["time"].strftime("%c")
+			amount=b["amount"]
+			print("on {} you withdrew{}".format(time,amount))
+			
+		for x in self.deposits:
+			time=x["time"].strftime("%c")
+			amount=x["amount"]
+			print("on {} you deposited{}".format(time,amount))
+
 		
 			
 
