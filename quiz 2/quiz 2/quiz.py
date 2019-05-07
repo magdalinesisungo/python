1fd23cd6c3fa0150993263a4db210979e6bafad7
Magdaline Sisungo <47854471+magdalinesisungo@users.noreply.github.com>
2019-05-07 19:18:28 +0300

    Add files via upload

---
+++ quiz 2/quiz 2/quiz.py
@@ -0,0 +1,49 @@
+
+tens = [1,2,3,4,5,6,7,8,9];
+t = tens*10;
+for t in tens:
+return(t)
+
+list comprehension= [2,3,4,6,8,10,12];
+s = list comprehension ** 2;
+for s in list comprehension:
+return (s)
+
+sorted = [a + b];
+a = [9,1,4,7,3]
+b = [5,2,6,8,0]
+for a,b in sorted:
+sorted.sort()
+
+
+range_sum = [0,9];
+if n >= 9
+let = range_sum +1
+for n in range_sum:
+return(n)
+
+largest = [3,4,5,6,7,8,9];
+for l in largest:
+return[l]
+
+smallest = [1,2,3,4,5,6];
+for s in smallest:
+return [x]
+
+
+my_modulus = [10,20];
+for m in my_module =%3
+return (n)
+
+
+
+
+student = [{'balance': 1000, 'name': 'Irene'}, {'balance': 2000, 'name': 'Pauline'}, 
+{'balance': 3000, 'name': 'Naima'}, {'balance': 1000, 'name': 'Rose'}];
+for name, balance in student();
+print = ("Hello {}, you balance is {}".format(name, balance))
+
+
+
+
+
